4adf68fa74b8a8d6b949c7c3c3a8b75f390c7e43
Leandromferandez <leandromfernandez@gmail.com>
2023-09-20 21:19:38 -0300

    DESAFIO STARK 02 FINALIZADO

--- desafio_stark_02/desafio_stark_02/desafio_stark_02.py
+++ desafio_stark_02/desafio_stark_02/desafio_stark_02.py
@@ -28,36 +28,46 @@ NOTA: Se debe aplicar el tema Funciones visto en clase para cada opción del men
 from funciones_data_02 import *
 from data_stark import lista_personajes
 
-
-#I. Listar todos los superhéroes agrupados por color de ojos.
-
-# Crear un diccionario vacío para agrupar personajes por tipo de inteligencia
-personajes_por_inteligencia = {}
-
-# Iterar a través de la lista de personajes y agruparlos por tipo de inteligencia
-for personaje in lista_personajes:
-    nombre = personaje["nombre"]
-    inteligencia = personaje["inteligencia"]
-        
-    if inteligencia in personajes_por_inteligencia:
-        personajes_por_inteligencia[inteligencia].append(nombre)
+while True:
+    print(f'Menu de opciones')
+    print(f'Opcion A - Imprimir el nombre de cada superhéroe de género NB')
+    print(f'Opcion B - Mostrar heroina mas alta.')
+    print(f'Opcion C - Recorrer la lista y determinar cuál es el superhéroe más alto de género M.')
+    print(f'Opcion D - Recorrer la lista y determinar cuál es el superhéroe más débil de género M')
+    print(f'Opcion E - Recorrer la lista y determinar cuál es el superhéroe más débil de género NB')
+    print(f'Opcion F - Recorrer la lista y determinar la fuerza promedio de los superhéroes de género NB')
+    print(f'Opcion G - Mostrar cuántos superhéroes tienen cada tipo de color de ojos.')
+    print(f'Opcion H - Mostrar cuántos superhéroes tienen cada tipo de color de pelo.')
+    print(f'Opcion I - Listar todos los superhéroes agrupados por color de ojos.')    
+    print(f'Opcion J - Listar todos los superhéroes agrupados por tipo de inteligencia')    
+    print(f'Opcion K - SALIR')    
+    respuesta =input('Ingrese la opcion deseada: ').upper()   
+    
+    if respuesta == "A":
+        mostar_heroes_nb(lista_personajes)
+    elif respuesta == "B":
+      mostrar_heroina_mas_alta(lista_personajes)
+    elif respuesta == "C":
+        mostrar_heroe_mas_alto(lista_personajes)        
+    elif respuesta == "D":
+        mostrar_heroe_mas_debil(lista_personajes)      
+    elif respuesta == "E":
+        mostrar_heroe_nb_debil(lista_personajes)
+    elif respuesta == "F":
+        muestrar_promedio_fuerzas_nb(lista_personajes)
+    elif respuesta == "G":
+        mostrar_tipo_color_ojos(lista_personajes)       
+    elif respuesta == "H":
+        mostrar_cantidad_colores_pelo(lista_personajes)
+    elif respuesta == "I":
+        muestra_grupos_color_ojos(lista_personajes)
+    elif respuesta == "J":
+        muestra_agrupados_por_inteligencia(lista_personajes)
+    elif respuesta == "K":
+        print(F'Hasta Pronto! ')
+        break
     else:
-        personajes_por_inteligencia[inteligencia] = [nombre]
-
-# Imprimir la lista de personajes agrupados por tipo de inteligencia
-for inteligencia, lista_personajes in personajes_por_inteligencia.items():
-    print(f"Tipo de inteligencia: {inteligencia}")
-    for personaje in lista_personajes:
-        print(f" - {personaje}")
-
-
-
-    
-    
-    
-
-
-    
+        print("Opcion no valida. Ingrese nuevamente!. ")
 
 
 
--- desafio_stark_02/desafio_stark_02/funciones_data_02.py
+++ desafio_stark_02/desafio_stark_02/funciones_data_02.py
@@ -1,15 +1,9 @@
-def mostar_heroes_nb(lista:list) -> list:
-    """ Recibe una lista de personajes - devuelve los nombre NB que se encuentren en la mista """
-    lista_nombre_nb = []
-    for personaje in lista:
-        if personaje['genero'] == "NB":        
-            nombre_nb = personaje['nombre']        
-            lista_nombre_nb.append(nombre_nb)
-    return lista_nombre_nb
-
 #A. Recorrer la lista imprimiendo por consola el nombre de cada superhéroe de género NB
 def mostar_heroes_nb(lista:list) -> list:
-    """ Recibe una lista de personajes - devuelve los nombre NB que se encuentren en la mista """
+    """
+    La función `mostar_heroes_nb` toma una lista de personajes y devuelve los nombres de los personajes
+    con el género "NB" en la lista.    
+    """
     lista_nombre_nb = []
     contador_nb = 0
     for personaje in lista:
@@ -26,6 +20,7 @@ def mostar_heroes_nb(lista:list) -> list:
 ####################################################################################################
 # B. Recorrer la lista y determinar cuál es el superhéroe más alto de género F
 def mostrar_heroina_mas_alta(lista:list): 
+    # El código busca la superheroína más alta (género "F") en la lista dada. Finalmente, imprime el nombre y la altura de la superheroína más alta.
     altura_femenino_max = None    
     for personaje in lista:
         if personaje['genero'] == "F":
@@ -33,11 +28,13 @@ def mostrar_heroina_mas_alta(lista:list):
             if altura_femenino_max == None or alturas > altura_femenino_max:
                 altura_femenino_max = alturas
                 nombre_femenino_max = personaje['nombre']
-    print(f'La heroina mas alta es {nombre_femenino_max} y su altura es: {altura_femenino_max} Cm.')
+    mensaje = print(f'La heroina mas alta es {nombre_femenino_max} y su altura es: {altura_femenino_max} Cm.')
+    return mensaje
 
 ####################################################################################################
 #C. Recorrer la lista y determinar cuál es el superhéroe más alto de género M
 def mostrar_heroe_mas_alto(lista:list):
+# El código busca el superhéroe con mayor altura entre aquellos con el género "M" en la lista dada. Finalmente, imprime el nombre y la altura del superhéroe masculino más alto.
     altura_masculino_max = None    
     for personaje in lista:
         if personaje['genero'] == "M":
@@ -45,11 +42,13 @@ def mostrar_heroe_mas_alto(lista:list):
             if altura_masculino_max == None or alturas > altura_masculino_max:
                 altura_masculino_max = alturas
                 nombre_masculino_max = personaje['nombre']
-    print(f'El heroe mas alto es {nombre_masculino_max} y su altura es: {altura_masculino_max} Cm.')
+    mensaje = print(f'El heroe mas alto es {nombre_masculino_max} y su altura es: {altura_masculino_max} Cm.')
+    return mensaje
 
 ####################################################################################################
 #D. Recorrer la lista y determinar cuál es el superhéroe más débil de género M
-def mostrar_heroe_mas_debil(lista:list): 
+def mostrar_heroe_mas_debil(lista:list):    
+    #Encuentra al superhéroe con menor fuerza entre aquellos con el género "M" en la lista dada    
     fuerza_min = None
     for personaje in lista:
         if personaje['genero'] == "M":
@@ -57,11 +56,13 @@ def mostrar_heroe_mas_debil(lista:list):
             if fuerza_min == None or fuerza < fuerza_min:
                 fuerza_min = fuerza
                 nombre_mas_debil = personaje['nombre']
-    print(f'El heroe mas debil es {nombre_mas_debil} y su fuerza es {fuerza_min}')
+    mensaje = print(f'El heroe mas debil es {nombre_mas_debil} y su fuerza es {fuerza_min}')
+    return mensaje
 
 ####################################################################################################
 #E. Recorrer la lista y determinar cuál es el superhéroe más débil de género NB
 def mostrar_heroe_nb_debil(lista:list):
+    # encuentra al superhéroe con menor fuerza ("fuerza") entre aquellos con el género "NB" en la lista dada.
     fuerza_min_nb = None
     contador_nb = 0
     for personaje in lista:
@@ -80,6 +81,7 @@ def mostrar_heroe_nb_debil(lista:list):
 ####################################################################################################
 #F. Recorrer la lista y determinar la fuerza promedio de los superhéroes de género NB
 def muestrar_promedio_fuerzas_nb(lista:list):
+   # El código la fuerza promedio de los superhéroes con el género "NB".   
     acumulador_fuerzas = 0
     contador_nb = 0
     for personaje in lista:
@@ -93,7 +95,12 @@ def muestrar_promedio_fuerzas_nb(lista:list):
     
 # G. Determinar cuántos superhéroes tienen cada tipo de color de ojos.            
 def mostrar_tipo_color_ojos(lista:list):
-    
+    """
+    El código cuenta la cantidad de superhéroes para cada color de ojos en la lista proporcionada. Crea
+    una lista vacía llamada `lista_color_ojos` para almacenar los colores de ojos de los superhéroes.
+    Luego, itera sobre cada superhéroe en la `lista` y recupera su color de ojos (`color_ojos`). El
+    color de ojos se adjunta a `lista_color_ojos`.
+    """    
     lista_color_ojos = []
     conteo_colores = {}
 
@@ -113,8 +120,8 @@ def mostrar_tipo_color_ojos(lista:list):
     return mensaje
 
 #H. Determinar cuántos superhéroes tienen cada tipo de color de pelo.
-def mostrar_cantidad_colores_pelo(lista:list):
-    
+def mostrar_cantidad_colores_pelo(lista:list):      
+    #Cuenta la cantidad de superhéroes para cada color de cabello en la lista proporcionada.    
     lista_color_pelo = []
     conteo_colores_pelo = {}
     for personaje in lista:
@@ -131,3 +138,45 @@ def mostrar_cantidad_colores_pelo(lista:list):
         mensaje = print(f'CON COLOR DE PELO {color} HAY {cantidad} SUPERHEROES')
     
     return mensaje   
+
+#I
+def muestra_grupos_color_ojos(lista: list):
+    """ 
+    El código está creando un diccionario llamado `color_ojos_personajes` para agrupar a los superhéroes
+    por su color de ojos. Si el color de ojos ya existe como clave en el diccionario,agrega el nombre a la lista correspondiente. 
+    Si el color de ojos aún no es una clave en el diccionario, crea un nuevo par clave-valor con el color de ojos como clave y una lista que contiene
+    el nombre como valor. Finalmente, imprime el color de ojos y la correspondiente lista de nombres.
+    """
+    color_ojos_personajes = {}
+    for personajes in lista:
+        color_ojos = personajes["color_ojos"]
+        nombre = personajes["nombre"]
+        if color_ojos in color_ojos_personajes:
+            color_ojos_personajes[color_ojos].append(nombre)
+        else:
+            color_ojos_personajes[color_ojos] = [nombre]    
+    for color, nombre in color_ojos_personajes.items():     
+       mensaje = print(f"los que tienen ojos {color} son: \n  {nombre}")
+    return mensaje
+
+#J
+def muestra_agrupados_por_inteligencia(lista:list):
+    """ 
+     El código está creando un diccionario llamado "inteligencia_personajes" para agrupar a los
+     superhéroes por su nivel de inteligencia.Si el nivel de inteligencia ya existe como clave en el diccionario, agrega el nombre a la lista
+     correspondiente.
+     Si el nivel de inteligencia aún no es una clave en el diccionario, crea un nuevo par clave-valor con el nivel de inteligencia como clave y una lista que contiene el nombre
+     como valor. Finalmente, imprime el nivel de inteligencia y la lista de nombres correspondiente. 
+     """
+    inteligencia_personajes = {}
+    for personajes in lista:
+        inteligencia = personajes["inteligencia"]
+        nombre = personajes["nombre"]
+        if inteligencia in inteligencia_personajes:
+            inteligencia_personajes[inteligencia].append(nombre)
+        else:
+            inteligencia_personajes[inteligencia] = [nombre]
+
+    for inteligencia, nombre in inteligencia_personajes.items():     
+        mensaje = print(f"los que tienen inteligencia tipo : {inteligencia} son: \n  {nombre}")
+    return mensaje
